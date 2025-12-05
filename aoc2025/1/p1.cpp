3f90cffb2298243118183938538f6d25c9984c63
aliu <andrew.yc.liu@gmail.com>
2025-12-05 12:32:28 -0800

    update with timing code

--- aoc2025/1/p1.cpp
+++ aoc2025/1/p1.cpp
@@ -1,14 +1,23 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <chrono>
 
 int main()
 {
     std::ifstream in("input.txt");
     std::string line;
+
+
+    std::vector<std::string> lines;
+    while (getline(in, line)) {
+        lines.push_back(line);
+    }
+    auto start = std::chrono::steady_clock::now();
+
     int dial = 50;
     int count = 0;
-    while (getline(in, line)) {
+    for (std::string line : lines) {
         int val = stoi(line.substr(1));
         if (line[0] == 'L') {
             dial -= val;
@@ -20,5 +29,8 @@ int main()
             count++;
         }
     }
+
+    auto end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us " << '\n';
     std::cout << count << '\n';
 }
--- aoc2025/1/p2.cpp
+++ aoc2025/1/p2.cpp
@@ -1,13 +1,23 @@
 #include <iostream>
+#include <vector>
 #include <fstream>
+#include <chrono>
 
 int main()
 {
     std::ifstream in("input.txt");
     std::string line;
+
+
+    std::vector<std::string> lines;
+    while (getline(in, line)) {
+        lines.push_back(line);
+    }
+    auto start = std::chrono::steady_clock::now();
+
     int dial = 50;
     int count = 0;
-    while (getline(in, line)) {
+    for (std::string line : lines) {
         int val = stoi(line.substr(1));
 
         if (line[0] == 'L') {
@@ -19,7 +29,10 @@ int main()
         }
         dial = (dial + 1000000) % 100; // big number so if really big left rotation it still mods correctly
 
-//        std::cout << line << ' ' << count << '\n';
+
     }
+
+    auto end = std::chrono::steady_clock::now();
+    std::cout << std::chrono::duration_cast<std::chrono::microseconds>(end - start).count() << "us " << '\n';
     std::cout << count << '\n';
 }
